aab290be4444f20590504455fd2b857fd5247fcc
hedi <hedi.cheikh@esprit.tn>
2026-02-16 09:56:20 +0100

    Pack & Inscription: initial work

--- src/main/java/Utiles/MyDB.java
+++ src/main/java/Utiles/MyDB.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class MyDB {
 
-    private final String URL = "jdbc:mysql://localhost:3306/ecoadventure";
+    private final String URL = "jdbc:mysql://localhost:3308/ecoadventure";
     private final String USER = "root";
     private final String PASSWORD = "";
 
